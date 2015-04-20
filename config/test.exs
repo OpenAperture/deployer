17db10bf36c8bf4b3140f3c69be019af42b70e7f
aandrieiev <andreev00@gmail.com>
2015-04-20 21:56:18 +0200

    Use a more descriptive config session name.

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :autostart,
   register_queues: true
 
-config :manager_api,
+config :openaperture_manager_api,
   manager_url:         System.get_env("MANAGER_URL")         || "https://openaperture-mgr.host.co",
   oauth_login_url:     System.get_env("OAUTH_LOGIN_URL")     || "https://auth.host.co",
   oauth_client_id:     System.get_env("OAUTH_CLIENT_ID")     || "id",
