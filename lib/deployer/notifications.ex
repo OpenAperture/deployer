5b6d8eac531a092162727c0d8d77487a9c4d1550
aandrieiev <andreev00@gmail.com>
2015-04-20 21:56:52 +0200

    Fix notification sending

--- lib/deployer/notifications.ex
+++ lib/deployer/notifications.ex
@@ -1,18 +1,33 @@
 defmodule OpenAperture.Deployer.Notifications do
   @moduledoc """
-  Describes sending notification messages to an AMQP broker.
+  Wraps sending notification messages to an AMQP broker.
   """
 
   use OpenAperture.Messaging
-  alias OpenAperture.Deployer.Configuration
+
+  alias OpenAperture.Deployer
+  alias Deployer.Configuration
+  alias Deployer.Notifications
+
+  alias OpenAperture.ManagerApi
+
+  alias OpenAperture.Messaging
+  alias Messaging.AMQP.QueueBuilder
+  alias Messaging.ConnectionOptionsResolver
 
   @doc """
-  Sends an actual message.
+  Sends an actual message to a the `notifications_hipchat` queue.
   Returns :ok if succeffully sent or :error otherwise.
   """
   @spec send(Map) :: :ok | :error
   def send(msg) do
-    case publish(Configuration.connection_options, "notifications_hipchat", msg) do
+    api                = ManagerApi.create!(Configuration.api_creds)
+    exchange           = Configuration.current_exchange_id
+    queue              = QueueBuilder.build(api, "noitification_hipchat", exchange)
+    connection_options =
+      ConnectionOptionsResolver.get_for_broker(api, Configuration.current_broker_id)
+
+    case Notifications.publish(connection_options, queue, msg) do
       :ok -> :ok
       _   -> :error
     end
