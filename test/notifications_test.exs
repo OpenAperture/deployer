524ee3113cd97f563fd16deadb737de5b990abcc
aandrieiev <andreev00@gmail.com>
2015-04-20 21:58:08 +0200

    Mock actual call to publish()

--- test/notifications_test.exs
+++ test/notifications_test.exs
@@ -4,6 +4,11 @@ defmodule Notifications.Test do
   alias OpenAperture.Deployer.Notifications
 
   test "send() properly sends the message out" do
+    :meck.new(Notifications, [:passthrough])
+    :meck.expect(Notifications, :publish, fn(_, _, _) -> :ok end)
+
     assert Notifications.send("sdkfjh") == :ok
+  after
+    :meck.unload(Notifications)
   end
 end
