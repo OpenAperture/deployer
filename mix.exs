d2ec72ce0614c119825e7eaebc8ae82ce6401bf0
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-05-22 11:26:54 -0500

    bump fleet and messaging

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule OpenAperture.Deployer.Mixfile do
       {:ex_doc, github: "elixir-lang/ex_doc", only: [:test]},
       {:markdown, github: "devinus/markdown", only: [:test]},    
       
-      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "e791d59c5bfaec9daa5cf7397401795ccd064a2a", override: true},
+      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "8c51d099ec79473b23b3c385c072e6bf2219fba7", override: true},
       {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "5d442cfbdd45e71c1101334e185d02baec3ef945", override: true},
       {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4d65d2295f2730bc74ec695c32fa0d2478158182", override: true},
       {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "2e63b7889c76f4d3b749146f3ebceb01702cf012", override: true},
