f55da6fb21c9ccb07aee067ac177c60e0a2bad91
msawka <matt.sawka@perceptivesoftware.com>
2015-08-13 11:22:46 -0500

    bump fleet version

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule OpenAperture.Deployer.Mixfile do
       {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "e48c52b98abc86f4404954e7b4c85b090e83c69c", override: true},
       {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "ab5334f276b308706a91e85ca27ba937bb02fb9f", override: true},
       {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4d65d2295f2730bc74ec695c32fa0d2478158182", override: true},
-      {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "47de1db816abe3fff34d0f7172a932b9b526fe5c", override: true},
+      {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "38f1bbb2fb7bd3fc546dac06641ea7880fe1187d", override: true},
       {:openaperture_workflow_orchestrator_api, git: "https://github.com/OpenAperture/workflow_orchestrator_api.git", ref: "c66fa165e9ee07250d264b4b63ce375692e2b7cc", override: true},
       {:timex, "~> 0.12.9"},
       {:fleet_api, "~> 0.0.15", override: true},
