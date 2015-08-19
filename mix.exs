08c8da1d2c026040b520c76630e48dcb13ee202b
msawka <matt.sawka@perceptivesoftware.com>
2015-08-19 09:45:16 -0500

    update dependencies to latest

--- mix.exs
+++ mix.exs
@@ -30,17 +30,17 @@ defmodule OpenAperture.Deployer.Mixfile do
       {:earmark, github: "pragdave/earmark", tag: "v0.1.8", only: [:test], override: true},    
       
       {:poison, "~>1.4.0", override: true},
-      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "e48c52b98abc86f4404954e7b4c85b090e83c69c", override: true},
-      {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "ab5334f276b308706a91e85ca27ba937bb02fb9f", override: true},
-      {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4d65d2295f2730bc74ec695c32fa0d2478158182", override: true},
-      {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "38f1bbb2fb7bd3fc546dac06641ea7880fe1187d", override: true},
-      {:openaperture_workflow_orchestrator_api, git: "https://github.com/OpenAperture/workflow_orchestrator_api.git", ref: "c66fa165e9ee07250d264b4b63ce375692e2b7cc", override: true},
-      {:timex, "~> 0.12.9"},
+      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "3d3a84eabf4ba0a3a827a61c4d99cdbf0ab49a0d", override: true},
+      {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "86cf2c324434f9899416881219e03c0f959c2896", override: true},
+      {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4b9146507ab50789fec4696b96f79642add2b502", override: true},
+      {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "324acdae0ceecb6a954d804d56d9d2fceaeb937c", override: true},
+      {:openaperture_workflow_orchestrator_api, git: "https://github.com/OpenAperture/workflow_orchestrator_api.git", ref: "df4033a048145c62361e7e161c60142b7fc501e4", override: true},
+      {:timex, "~> 0.13.3", override: true},
       {:fleet_api, "~> 0.0.15", override: true},
 
       #test dependencies
       {:exvcr, github: "parroty/exvcr", only: :test},
-      {:meck, "0.8.2", only: :test}
+      {:meck, "0.8.3", override: true},
     ]
   end
 end
